78a4a7f294c5ef00d3ead3a93a4be52348d78a50
Brody May <brodymay2019@outlook.com>
2023-02-17 11:04:06 -0500

    finished

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace StaticExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using StaticExercise;
+
+var answer = TempConverter.FahrenheitToCelcius(87);
+Console.WriteLine(answer);
+
+
+var answer2 = TempConverter.CelciusToFahrenheit(0);
+Console.WriteLine(answer2);
+
+Console.WriteLine("Enter in Fahrenheit to be converted to Celcius");
+var userInput = double.Parse(Console.ReadLine());
+
+var answer3 = TempConverter.FahrenheitToCelcius(userInput);
+Console.WriteLine($"{userInput} degrees Fahrenheit is {answer3} degrees Celcius");
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal static class TempConverter
+    {
+        public static double FahrenheitToCelcius(double fTemp) 
+        { 
+            return (fTemp - 32) * 5 / 9;
+        }
+        public static double CelciusToFahrenheit(double cTemp) 
+        {
+            return (cTemp * 9 / 5 + 32);
+        }
+    }
+}
